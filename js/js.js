3d6cd6498d19b539b41ce35e361dbbc090db466b
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-09 22:13:10 +0200

    adding meta tags to the website

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".typed", {
     strings: ["Frontend Developer", "Software Engineer"],
     typeSpeed: 100,
-    backSpeed: 100,
+    backSpeed: 50,
     backDelay: 1000,
     loop: true
 })
