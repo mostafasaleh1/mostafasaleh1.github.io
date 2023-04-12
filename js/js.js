2c1085f4595ec6dc1ba8f241dd0d39db43f53c11
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-12 17:35:31 +0200

    adding the feature of highlighting the active class navbar link

--- js/js.js
+++ js/js.js
@@ -11,15 +11,20 @@ var typed = new Typed(".typed", {
 /* ACTIVE CLASS */
 window.addEventListener("scroll", function () { //making the window do the function below while scrolling, this fucntion cotinuously checks if the section is in the vieport or not. If yes it adds the active class, if not it remove the active class.
     let secArr = document.querySelectorAll('section'); // creating an array that contains all the section on the document.
+    let navlinks = document.querySelectorAll(".navbar-links");
     secArr.forEach(function (SecInViewport) { // looping over the scetion in the viewport and checking continuously if it's existing or not.
-
+        let id = SecInViewport.getAttribute("id");
         let bounds = SecInViewport.getBoundingClientRect(); // getting the dimentions of the rectangle of the section being looped and storing it into a variable called bound.
 
 //these are the dimensions that determine if that section is 75% in the viewport or not.
 
         if (bounds.top <= 350.00 && bounds.top >= -350.00) { // checking if the rectangle is in the viewport or not.
-            if (!SecInViewport.classList.contains("your-active-class")) { //checking if the rectangle doesn't contain the activ class.
+            if (!SecInViewport.classList.contains("active-class")) { //checking if the rectangle doesn't contain the activ class.
                 SecInViewport.classList.add("active-class"); //if the active class doesn't exist and the seciton is in the viewport, it'll add the class as an attribute to the sction in the viewport.
+                navlinks.forEach(link => {
+                    link.classList.remove("active");
+                    document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+                });
             }
         } 
         else {
