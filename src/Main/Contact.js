d01f420c51f8f5ff2425cc0eb1afea76fb7e4224
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-20 20:51:29 +0200

    enabling contact form and making the zoomIn css effect faster

--- src/Main/Contact.js
+++ src/Main/Contact.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_bugcxhd', 'template_gg0bfq4', form.current, 'BVRhTT-DBkT3hDcnl')
+            .then((result) => {
+                alert(result.text);
+            }, (error) => {
+                alert(error.text);
+            });
+    };
+
     return (
         <section className="contact" id="contact">
             <h2 className="heading">Contact <span>Me</span></h2>
-            <form action="#">
+            <form action='#' ref={form} onSubmit={sendEmail}>
                 <div className="input-box">
-                    <input type="text" placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
+                    <input type="text" placeholder="Full Name" name='from_name' />
+                    <input type="email" placeholder="Email Address" name='email' />
                 </div>
                 <div className="input-box">
-                    <input type="number" placeholder="Mobile Number" />
-                    <input type="text" placeholder="Email Subject" />
+                    <input type="number" placeholder="Mobile Number" name='phone' />
+                    <input type="text" placeholder="Email Subject" name='subject' />
                 </div>
-                <textarea name="textarea" id="text-area" cols="30" rows="10" placeholder="Your Message" style={{ resize: "none" }}></textarea>
+                <textarea name="message" id="text-area" cols="30" rows="10" placeholder="Your Message" style={{ resize: "none" }}></textarea>
                 <input type="submit" value="Send Message" className="btn" />
             </form>
         </section>
