f05ac1bf64ba0659c84b49d5a7ff4b2528faf22b
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-25 14:20:52 +0200

    partially working navbar and hamburger icon when clicking on the hamburger icon to close the navbar, the navbar hides and then closes. to solve this, i need to remove the class "first-off" when the hamburger icon is clicked. when clicking on the navbar links, the navbar must close immediately, this must be fixed in the next commit.

--- src/Main/header/Header.jsx
+++ src/Main/header/Header.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 function Header() {
-    return (
-        <header className="header">
-            <i className='bx bx-menu' id="menu-icon"></i>
-            <nav className="navbar off first-off">
-                <a href="#home" className="navbar-links active" title="home">Home</a>
-                <a href="#about" className="navbar-links" title="about">About</a>
-                <a href="#services" className="navbar-links" title="services">Services</a>
-                <a href="#portfolio" className="navbar-links" title="portfolio">Portfolio</a>
-                <a href="#contact" className="navbar-links" title="contact">Contact</a>
-            </nav>
-        </header>
-    );
+  const [isTheMenuOpen, setIsTheMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsTheMenuOpen(!isTheMenuOpen);
+  }
+
+  const navLinks = [
+    <a href="#home" className="navbar-links active" title="home">Home</a>,
+    <a href="#about" className="navbar-links" title="about">About</a>,
+    <a href="#services" className="navbar-links" title="services">Services</a>,
+    <a href="#portfolio" className="navbar-links" title="portfolio">Portfolio</a>,
+    <a href="#contact" className="navbar-links" title="contact">Contact</a>,
+  ];
+
+  const navbar = <nav className={isTheMenuOpen ? "navbar on" : "navbar off first-off"} >{navLinks}</nav>;
+  const menuIcon = <i className={isTheMenuOpen ? "bx bx-x" : 'bx bx-menu'} id="menu-icon" onClick={toggleMenu}></i>;
+
+  return (
+    <header className="header">
+      {menuIcon}
+      {navbar}
+    </header>
+  );
 }
 
 export default Header;
