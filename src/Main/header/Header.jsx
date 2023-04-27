6e05dee0ca5bbe7295f09c30d066ff17a1f412b0
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-27 15:29:39 +0200

    enabling active class function

--- src/Main/header/Header.jsx
+++ src/Main/header/Header.jsx
@@ -3,8 +3,9 @@ import { BsFillClipboardDataFill } from "react-icons/bs";
 import { HiHome } from "react-icons/hi";
 import { FaUser } from "react-icons/fa";
 import { IoConstructSharp, IoLanguage } from "react-icons/io5";
-import {AiTwotonePhone} from "react-icons/ai"
+import { AiTwotonePhone } from "react-icons/ai"
 import "./header.css";
+
 
 function Header() {
   const [isTheMenuOpen, setIsTheMenuOpen] = useState(false);
@@ -21,8 +22,9 @@ function Header() {
     <a href="#contact" className="navbar-links" title="contact" onClick={toggleMenu}><AiTwotonePhone /></a>,
   ];
 
-  const navbar = <nav className={isTheMenuOpen ? "navbar on" : "navbar off first-off"} >{navLinks}</nav>;
+  const navbar = <nav className={isTheMenuOpen ? "navbar on" : "navbar off"} >{navLinks}</nav>;
   const menuIcon = <i className={isTheMenuOpen ? "bx bx-x" : 'bx bx-menu'} id="menu-icon" onClick={toggleMenu}></i>;
+
 
   return (
     <header className="header">
@@ -33,3 +35,29 @@ function Header() {
 }
 
 export default Header;
+
+
+
+// ACTIVE CLASS AND ACTIVE LINKS
+var navLinks = document.querySelectorAll(".navbar-links");
+
+window.addEventListener("scroll", function () { 
+    let secArr = document.querySelectorAll('section');
+    secArr.forEach(function (SecInViewport) {
+        let id = SecInViewport.getAttribute("id");
+        let bounds = SecInViewport.getBoundingClientRect();
+
+        if (bounds.top <= 350.00 && bounds.top >= -350.00) {
+            if (!SecInViewport.classList.contains("active-class")) {
+                SecInViewport.classList.add("active-class");
+                navLinks.forEach(link => {
+                    link.classList.remove("active");
+                    document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+                });
+            }
+        } 
+        else {
+            SecInViewport.classList.remove("active-class");
+        }
+    });
+});
