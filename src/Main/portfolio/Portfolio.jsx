cc7e81439556b05ed2c7c97f74d41c6f5a4df0e4
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-28 17:00:27 +0300

    adding buttons to portfolio layer and fixing text layout

--- src/Main/portfolio/Portfolio.jsx
+++ src/Main/portfolio/Portfolio.jsx
@@ -14,56 +14,86 @@ function Portfolio() {
             <h2 className="heading">Latest <span>Project</span></h2>
             <div className="protfolio-container">
                 <div className="portfolio-box">
-                    <img src = {portfolio1} alt="portfolio1" />
+                    <img src = {portfolio1} alt="the first website i've developed" />
                     <div className="portfolio-layer">
-                        <h4>Web Design</h4>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, sequi.</p>
-                        <a href="#"><i className='bx bx-link-external' title="#"></i></a>
+                        <div className="portfolio-layer-text-container">
+                        <h4>Mint Mind Coffee company</h4>
+                        <p>Mint Mind coffee company is a clone website of the Starbucks coffee company's website. I developed this for a Russian client in my free time.</p>
+                        </div>
+                        <div className="portfolio-links-container">
+                        <a href="https://github.com/mostafasaleh1/mintmind" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bxl-github' title="View on Github"></i> Github</a>
+                        <a href="https://mostafasaleh1.github.io/mintmind" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bx-broadcast' title="Open Live Demo"></i> Live Demo</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="portfolio-box">
                     <img src={portfolio2} alt="portfolio1" />
                     <div className="portfolio-layer">
+                        <div className="portfolio-layer-text-container">
                         <h4>Web Design</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, sequi.</p>
-                        <a href="#"><i className='bx bx-link-external' title="#"></i></a>
+                        </div>
+                        <div className="portfolio-links-container">
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bxl-github' title="View on Github"></i> Github</a>
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bx-broadcast' title="Open Live Demo"></i> Live Demo</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="portfolio-box">
                     <img src={portfolio3} alt="portfolio1" />
                     <div className="portfolio-layer">
+                        <div className="portfolio-layer-text-container">
                         <h4>Web Design</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, sequi.</p>
-                        <a href="#"><i className='bx bx-link-external' title="#"></i></a>
+                        </div>
+                        <div className="portfolio-links-container">
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bxl-github' title="View on Github"></i> Github</a>
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bx-broadcast' title="Open Live Demo"></i> Live Demo</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="portfolio-box">
                     <img src={portfolio4} alt="portfolio1" />
                     <div className="portfolio-layer">
+                        <div className="portfolio-layer-text-container">
                         <h4>Web Design</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, sequi.</p>
-                        <a href="#"><i className='bx bx-link-external' title="#"></i></a>
+                        </div>
+                        <div className="portfolio-links-container">
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bxl-github' title="View on Github"></i> Github</a>
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bx-broadcast' title="Open Live Demo"></i> Live Demo</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="portfolio-box">
                     <img src={portfolio5} alt="portfolio1" />
                     <div className="portfolio-layer">
+                        <div className="portfolio-layer-text-container">
                         <h4>Web Design</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, sequi.</p>
-                        <a href="#"><i className='bx bx-link-external' title="#"></i></a>
+                        </div>
+                        <div className="portfolio-links-container">
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bxl-github' title="View on Github"></i> Github</a>
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bx-broadcast' title="Open Live Demo"></i> Live Demo</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="portfolio-box">
                     <img src={portfolio6} alt="portfolio1" />
                     <div className="portfolio-layer">
+                        <div className="portfolio-layer-text-container">
                         <h4>Web Design</h4>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, sequi.</p>
-                        <a href="#"><i className='bx bx-link-external' title="#"></i></a>
+                        </div>
+                        <div className="portfolio-links-container">
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bxl-github' title="View on Github"></i> Github</a>
+                        <a href="#" className="portfolio-layer-link btn" target="_blank" rel="noreferrer"><i className='bx bx-broadcast' title="Open Live Demo"></i> Live Demo</a>
+                        </div>
                     </div>
                 </div>
             </div>
