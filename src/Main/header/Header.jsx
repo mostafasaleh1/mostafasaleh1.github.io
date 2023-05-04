fc3797f6e6d5b61f6532a75ac24873ee278ef6e5
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-05-04 21:32:39 +0300

    adding special key to each element in the navbar to fix the keys error, fixing zindex in services section, and fixing footer's huge margin in mobile view

--- src/Main/header/Header.jsx
+++ src/Main/header/Header.jsx
@@ -16,15 +16,19 @@ function Header() {
   }
 
   const navLinks = [
-    <a href="#home" className="navbar-links active" title="home" onClick={toggleMenu}><HiHome /></a>,
-    <a href="#about" className="navbar-links" title="about" onClick={toggleMenu}><FaUser /></a>,
-    <a href="#services" className="navbar-links" title="services" onClick={toggleMenu}><BsFillClipboardDataFill /></a>,
-    <a href="#portfolio" className="navbar-links" title="portfolio" onClick={toggleMenu}><IoConstructSharp /></a>,
-    <a href="#skills" className="navbar-links" title="skills" onClick={toggleMenu}><IoBriefcase /></a>,
-    <a href="#contact" className="navbar-links" title="contact" onClick={toggleMenu}><AiTwotonePhone /></a>,
+    <a key={1} href="#home" className="navbar-links active" title="home" onClick={toggleMenu}><HiHome /></a>,
+    <a key={2} href="#about" className="navbar-links" title="about" onClick={toggleMenu}><FaUser /></a>,
+    <a key={3} href="#services" className="navbar-links" title="services" onClick={toggleMenu}><BsFillClipboardDataFill /></a>,
+    <a key={4} href="#portfolio" className="navbar-links" title="portfolio" onClick={toggleMenu}><IoConstructSharp /></a>,
+    <a key={5} href="#skills" className="navbar-links" title="skills" onClick={toggleMenu}><IoBriefcase /></a>,
+    <a key={6} href="#contact" className="navbar-links" title="contact" onClick={toggleMenu}><AiTwotonePhone /></a>
   ];
 
-  const navbar = <nav className={isTheMenuOpen ? "navbar on" : "navbar"} >{navLinks}</nav>;
+  const outputLinks = navLinks.map(Link => {
+    return Link;
+  });
+
+  const navbar = <nav className={isTheMenuOpen ? "navbar on" : "navbar"} >{outputLinks}</nav>;
 
   return (
     <header className="header">
