ec102007a2a6938b431000dd5c20178e6b9286aa
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-08-18 15:06:01 +0300

    updating cv

--- src/pages/cv/cvapp/components/primary/leftmaincomponent/LeftMainComponent.jsx
+++ src/pages/cv/cvapp/components/primary/leftmaincomponent/LeftMainComponent.jsx
@@ -4,54 +4,54 @@ import { SectionTitle, JobTitle, InstitutionName, DateAndLocation, BulletPoint }
 
 const LeftMainComponent = () => (
     <div className='cv-left'>
+        {/* Experience */}
+
         <SectionTitle content="experience" />
+
         <div className='cv-content-box'>
-            <JobTitle content="React Developer" />
-            <InstitutionName content="Google" />
-            <DateAndLocation date="30/5" location="Location1" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
+            <JobTitle content="Freenancer" />
+            <InstitutionName content="Upwork" />
+            <DateAndLocation date="15 Aug 2023 - Now" location="Alexandria, Egypt" />
+            <BulletPoint content="I'm currently working as a front-end developer on Upwork." />
         </div>
 
         <div className='cv-content-box'>
-            <SectionTitle content="projects" />
-            <JobTitle content="React Developer" />
-            <InstitutionName content="Google" />
-            <DateAndLocation date="30/5" location="Location1" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
+            <JobTitle content="Intern" />
+            <InstitutionName content="Telecom Egypt" />
+            <DateAndLocation date="3 Jul 2021 - 12 Aug 2021" location="Alexandria, Egypt" />
+            <BulletPoint content="I was a trainee at Telecom Egypt Miami Exchange. I learned about: Optical fibre cables, their structure, how they're connected underground, how to splice them using (Fusion Splicing), FTTH networks, Telecom Egypt traffic devices especially PCM, WDH, SDH and WDM." />
+        </div>
+
+        {/* Projects */}
+
+        <div className='cv-content-box'>
+            <SectionTitle content="Projects" />
+            <JobTitle content="Intelligent Mobile Robot" />
+            <InstitutionName content="Kfs-HIET" />
+            <DateAndLocation date="Jun 2022 - Jul 2023" location="Kafr Elsheikh, Egypt" />
+            <BulletPoint content="This is my Graduation Project, An autonomous indoor mobile robot, named IMR, with an arm for assisting tasks, uses odometry and Kinect v2 for SLAM mapping. It operates autonomously via AMCL, supports ROS, and future work includes object detection, mapping, and gesture control." />
         </div>
 
         <div className='cv-content-box'>
-            <JobTitle content="React Developer" />
-            <InstitutionName content="Google" />
-            <DateAndLocation date="30/5" location="Location1" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah sjkadfhsdfaf asdfj hsadf" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
+            <JobTitle content="Pathological Phenomena of Poultry" />
+            <InstitutionName content="AIOT" />
+            <DateAndLocation date="3 Jul 2021 - 12 Aug 2021" location="Kafr Elsheikh, Egypt" />
+            <BulletPoint content="I worked on the dataset to train a model to recognize sick broilers using deep learning and computer vision. The AI detects broilers and track them and differentiates among sick and healthy broilers." />
         </div>
 
         <div className='cv-content-box'>
-            <JobTitle content="React Developer" />
-            <InstitutionName content="Google" />
-            <DateAndLocation date="30/5" location="Location1" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
+            <JobTitle content="My Profile" />
+            <InstitutionName content="Personal Project" />
+            <DateAndLocation date="May 2023 - June 2023" location="Home" />
+            <BulletPoint content="I developed a Profile React App which works as a personal portfolio, blog and online CV and the most difficult challenge I faced in this project is the translation feature." />
+            <BulletPoint content="I used React.js, vanilla CSS and i18next which is an internationalisation library for react." />
         </div>
 
         <div className='cv-content-box'>
-            <JobTitle content="React Developer" />
+            <JobTitle content="Graduation project website" />
             <InstitutionName content="Google" />
-            <DateAndLocation date="30/5" location="Location1" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
-        </div>
-
-        <div className='cv-content-box'>
-            <JobTitle content="React Developer" />
-            <InstitutionName content="Google" />
-            <DateAndLocation date="30/5" location="Location1" />
-            <BulletPoint content="adkjfhdaskfjhsadlkfjshadlkf hsdfjkah kasjdhf kah" />
+            <DateAndLocation date="15 May 2023 - 30 May 2023" location="Home" />
+            <BulletPoint content="This is a profile website for my graduation project, I developed it using React, Vite and TailwindCSS." />
         </div>
     </div>
 );
--- src/pages/cv/cvapp/components/primary/rightmaincomponent/RightMainComponent.jsx
+++ src/pages/cv/cvapp/components/primary/rightmaincomponent/RightMainComponent.jsx
@@ -24,6 +24,7 @@ const RightMainComponent = () => (
                 <Skill content="Redux" containsLink={false} link="#" />
                 <Skill content="tailwindcss" containsLink={false} link="#" />
                 <Skill content="i18next" containsLink={false} link="#" />
+                <Skill content="Sass" containsLink={false} link="#" />
                 <Skill content="CCNA" containsLink={false} link="#" />
                 <Skill content="C" containsLink={false} link="#" />
                 <Skill content="C++" containsLink={false} link="#" />
@@ -42,20 +43,28 @@ const RightMainComponent = () => (
             </Skills>
         </div>
 
+        {/* Courses */}
+
         <div className='cv-content-box'>
             <SectionTitle content="Courses" />
             <BulletPoint content="Front End Web Development Professional Nanodegree" course={true} courseType="(Udacity Scholarship)" containsLink={true} link="#" />
-            <BulletPoint content="CCNA 200-301" course={true} courseType="(self-learning)" containsLink={true} link="#" />
+            <BulletPoint content="CCNA 200-301" course={true} courseType="(SNL Remote Course)" containsLink={true} link="#" />
+            <BulletPoint content="Widnows Server Adminstration" course={true} courseType="(Udemy)" containsLink={true} link="#" />
+            <BulletPoint content="Advanced ReactJS" course={true} courseType="(Coursera)" containsLink={true} link="#" />
+            <BulletPoint content="NodeJS" course={true} courseType="(Udemy)" containsLink={true} link="#" />
             <BulletPoint content="Fundamentals of Operating Systems" course={true} courseType="(self-learning)" containsLink={true} link="#" />
-            <BulletPoint content="Artificial Neural Networks" course={true} courseType="(Course)" containsLink={true} link="#" />
-            <BulletPoint content="Introduction to Computer Architecture using RISC-V" course={true} courseType="(Course)" containsLink={true} link="#" />
+            <BulletPoint content="Artificial Neural Networks" course={true} courseType="(HIET)" containsLink={true} link="#" />
+            <BulletPoint content="Introduction to Computer Architecture using RISC-V" course={true} courseType="(self-learning)" containsLink={true} link="#" />
         </div>
+
+        {/* Languages */}
 
         <div className='cv-content-box'>
             <SectionTitle content="Languages" />
             <BulletPoint content="Arabic" course={true} courseType="(Native)" containsLink={false} link="#" />
             <BulletPoint content="English" course={true} courseType="(Fluent)" containsLink={false} link="#" />
             <BulletPoint content="Russian" course={true} courseType="(Beginner)" containsLink={false} link="#" />
+            <BulletPoint content="German" course={true} courseType="(Beginner)" containsLink={false} link="#" />
         </div>
     </div>
 );
