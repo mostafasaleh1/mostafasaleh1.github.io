91bdb5d9267c0d1b7e089eb51e20d21170ef63a8
Mostafa Saleh <mostafasaleh2055@gmail.com>
2025-03-25 07:00:21 +0200

    updated CV download link3

--- src/Main/home/Home.jsx
+++ src/Main/home/Home.jsx
@@ -5,6 +5,7 @@ import { socialMediaData } from "../Data";
 import { useTranslation } from 'react-i18next';
 import { mainActiveLang } from "../../languages/language/ActiveLang";
 import { isRTL, RTL } from "../../languages/language/RTL";
+import { CVdownloadLink } from "../../CVdownloadLink";
 import "./home.css";
 
 const Home = () => {
@@ -42,7 +43,7 @@ const Home = () => {
                             })}
                         </div>
 
-                        <a href="https://my.microsoftpersonalcontent.com/personal/317e50c5e6fb11ca/_layouts/15/download.aspx?UniqueId=95bca8a4-5945-409c-a3a0-7fc50e2d2a7f&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiIxM2YyZmY3MC04NTYyLTQzYWItODBlNC1hZjU1YzY4MTNlNTciLCJhcHBpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDA0ODE3MTBhNCIsImF1ZCI6IjAwMDAwMDAzLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMC9teS5taWNyb3NvZnRwZXJzb25hbGNvbnRlbnQuY29tQDkxODgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsImV4cCI6IjE3NDI4MTEwNzEifQ.KHOccNF-JXMr66Ib0fT1n_j21wrT6oQWARgz-5Yr2bOpbWkT3L5mC5CR7oiOnDeoQ0IVdlWOvFJAYjfbOwgOFBhmQbGkhl2eKucP_IWQUK5c8rnLwooFxru2QHs1ZAI7eIMbF13YE5bU1ywr_KJQAOHgRsnhB-eEYOoDxQkIhWil7lTWjaoQ6oMyR-PB4mQuW3nIgkV_Hh0z-nQTkk8JuFylyfZnXxNLnjxk8TmiYwT1WAGaLSnsMPkR8gfzw5WqmRS5b5iNCxnaCSUhZyO8Rw31xW_8BglBIPiFCDaxI0p-bUgtV6u-01mownbsvTVZlXRLdYR5C_43T7ar2CXRf5E6-tcst11zRqnhjPPMi5UbzZTyxcbCGAxL-Inf3LqbF5NBS-9n3kyHVdCBiCkUxFKNq9uKxF_f-RbMe3lNLC0.yaOolrvnARyuevIa9dy9yFYr6U49H-eMId8PTTjZON8&ApiVersion=2.0&AVOverride=1" className="btn" title={t(`${mainActiveLang()}.home.downloadcv`)}>{t(`${mainActiveLang()}.home.downloadcv`)}</a>
+                        <a href={CVdownloadLink} className="btn" title={t(`${mainActiveLang()}.home.downloadcv`)}>{t(`${mainActiveLang()}.home.downloadcv`)}</a>
                     </div>
                 </div>
             </main>
--- src/pages/cv/CV.js
+++ src/pages/cv/CV.js
@@ -3,6 +3,7 @@ import Header from "./cvapp/components/primary/header/Header";
 import MainContainer from "./cvapp/MainContainer";
 import { cvActiveLang } from "../../languages/language/ActiveLang";
 import { useTranslation } from 'react-i18next';
+import { CVdownloadLink } from "../../CVdownloadLink";
 import Footer from "../../Main/footer/Footer";
 import "./cv.css";
 
@@ -18,7 +19,7 @@ const CV = () => {
                 <MainContainer />
             </div>
         </div>
-        <a href="https://my.microsoftpersonalcontent.com/personal/317e50c5e6fb11ca/_layouts/15/download.aspx?UniqueId=95bca8a4-5945-409c-a3a0-7fc50e2d2a7f&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiIxM2YyZmY3MC04NTYyLTQzYWItODBlNC1hZjU1YzY4MTNlNTciLCJhcHBpZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDA0ODE3MTBhNCIsImF1ZCI6IjAwMDAwMDAzLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMC9teS5taWNyb3NvZnRwZXJzb25hbGNvbnRlbnQuY29tQDkxODgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsImV4cCI6IjE3NDI4MTEwNzEifQ.KHOccNF-JXMr66Ib0fT1n_j21wrT6oQWARgz-5Yr2bOpbWkT3L5mC5CR7oiOnDeoQ0IVdlWOvFJAYjfbOwgOFBhmQbGkhl2eKucP_IWQUK5c8rnLwooFxru2QHs1ZAI7eIMbF13YE5bU1ywr_KJQAOHgRsnhB-eEYOoDxQkIhWil7lTWjaoQ6oMyR-PB4mQuW3nIgkV_Hh0z-nQTkk8JuFylyfZnXxNLnjxk8TmiYwT1WAGaLSnsMPkR8gfzw5WqmRS5b5iNCxnaCSUhZyO8Rw31xW_8BglBIPiFCDaxI0p-bUgtV6u-01mownbsvTVZlXRLdYR5C_43T7ar2CXRf5E6-tcst11zRqnhjPPMi5UbzZTyxcbCGAxL-Inf3LqbF5NBS-9n3kyHVdCBiCkUxFKNq9uKxF_f-RbMe3lNLC0.yaOolrvnARyuevIa9dy9yFYr6U49H-eMId8PTTjZON8&ApiVersion=2.0&AVOverride=1" className="btn" type="button" title="Download CV">{t(`${cvActiveLang()}.cv.download`)}</a>
+        <a href="CVdownloadLink" className="btn" type="button" title="Download CV">{t(`${cvActiveLang()}.cv.download`)}</a>
             </section>
             <Footer />
     </React.Fragment>
